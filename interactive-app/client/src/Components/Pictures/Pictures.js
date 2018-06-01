bf709d1b4eb8560a930e780a0db5952b9808565e
devatha k <k.n.devs@gmail.com>
2018-06-01 15:32:01 -0400

    added images

--- interactive-app/client/src/Components/Pictures/Pictures.js
+++ interactive-app/client/src/Components/Pictures/Pictures.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import * as React from 'react';
+
 import Radium from 'radium';
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Row, Col, Container} from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,CardColumns, CardTitle, CardSubtitle, Row, Col, Container} from 'reactstrap';
+
+import Masonry from 'react-masonry-component';
+
+
+
 import './Pictures.css';
 
 
 
-
+// style related
 const textStyle = {
   // backgroundColor: 'green',
   ':hover': {
@@ -35,34 +41,43 @@ const cardStyle = {
 
 
 
+
+
+
 const Pictures = (props) => {
 
-	const pictures = props.dummyPictureData;
-  const listItemsPictures = pictures.map((picture, index) =>
-        <Col style={rowStyle} xs="4">
-          <Card style={cardStyle} className="cardstyleHover" key={picture.id}>
-            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-            <CardBody>
-              <CardTitle style={textStyle}>{picture.name}</CardTitle>
-              <CardSubtitle>{picture.profession}</CardSubtitle>
-              <CardText>Some quick example</CardText>
-            </CardBody>
-          </Card> 
-        </Col>
-  )
+    const pictures = props.dummyPictureData;
 
+    pictures.map((picture, index) =>{
+      console.log(picture.preview_photos)
+    } )
+
+
+    const listItemsPictures = pictures.map((picture, index) =>
+      picture.preview_photos.map((p,i)=>
+
+        <Card style={cardStyle} className="cardstyleHover" key={picture.id}>
+          <CardImg top width="100%" src={p.urls.regular} />
+          <CardBody>
+            <CardTitle style={textStyle}>{picture.name}</CardTitle>
+            <CardSubtitle>{picture.profession}</CardSubtitle>
+            <CardText>
+            </CardText>
+          </CardBody>
+        </Card> 
+      )
+    )
 
 
   return (
     <div>
-        <Container>
-          <br /><br />
-          <Row style={colStyle}>  
-            {listItemsPictures}
-          </Row>
-          <br /><br />
-
-        </Container>
+      <Container>
+        <br /><br />
+        <CardColumns>
+          {listItemsPictures}
+        </CardColumns>
+        <br /><br />
+      </Container>
     </div>
   );
 
--- interactive-app/client/src/Container/App.js
+++ interactive-app/client/src/Container/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+
 import Radium from 'radium';
-import Unsplash from 'unsplash-js';
 import axios from 'axios';
+import Unsplash, { toJson } from 'unsplash-js';
+import Modal from 'react-responsive-modal';
+
 
 
 import './App.css';
+
 
 import SiteNavbar from '../Dumbbells/Navbar'
 import SiteFooter from '../Dumbbells/Footer'
@@ -16,54 +20,87 @@ import Recommendations from '../Components/Recommendations/Recommendations';
 
 
 
-
 class App extends Component {
-  state = {
-    response: '',
-    pictureDummys:[
-      {id:'ayhi', name: 'John', profession:"Web Developer" },
-      {id:'ewsm', name: 'Sal', profession:"Front End" },
-      {id:'djrf', name: 'Nick', profession:"Back End" },
-      {id:'azhi', name: 'Jake', profession:"Web Developer" },
-      {id:'ersm', name: 'Robert', profession:"Front End" },
-      {id:'darf', name: 'Matt', profession:"Back End" },
-      {id:'ahhi', name: 'Max', profession:"Web Developer" },
-      {id:'ewtm', name: 'Joe', profession:"Front End" },
-      {id:'derf', name: 'Eric', profession:"Back End" }
-    ],
-    recomendationDummys:[
-      {id:'ayri', name: 'George', profession:"Web Developer" },
-      {id:'ewsm', name: 'Thomas', profession:"Front End" },
-      {id:'drrf', name: 'Jared', profession:"Back End" }
-    ]
 
-  };
+  constructor() {
+    super();
+    this.state = {
+      allsearchImages: [],
+      open: false
+    }
+  }
+
+
 
 
 
   componentDidMount() {
+    // use express proxy
     this.callApi()
       .then(res => this.setState({ response: res.express }))
       .catch(err => console.log(err));
-  }
+    //////////////////////////
+
+
+
+     // use unsplashjs
+    const unsplash = new Unsplash({
+      applicationId: "",
+      secret: "",
+      callbackUrl: "http://localhost:3000/"
+    });
+
+
+    unsplash.search.collections("flowers", 1, 60)
+    .then(toJson)
+    .then(json => {
+      console.log(json.results);
+      this.setState({ allsearchImages:json.results });
+    });
+    //////////////////////////
+  }; 
+
+     
 
 
   callApi = async () => {
     const response = await fetch('/api/hello');
     const body = await response.json();
-
     if (response.status !== 200) throw Error(body.message);
-
     return body;
   };
 
 
+
+  // eventHandlers for
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+ 
+
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+  //////////////////////////
+
+
+
+
+
   render() {
+    const { open } = this.state;
+
     return (
       <div className="App">
       <SiteNavbar/>
-          <Pictures dummyPictureData= {this.state.pictureDummys}/>
-          <Recommendations dummyRecommendationData = {this.state.recomendationDummys} />
+          <button onClick={this.onOpenModal}>Open modal</button>
+          <Modal open={open} onClose={this.onCloseModal} center>
+            <h2>Simple centered modal</h2>
+          </Modal>
+          <Pictures dummyPictureData = {this.state.allsearchImages}/>
+          {/*<Recommendations dummyRecommendationData = {this.state.recomendationDummys} />*/}
+
           <SiteFooter />
       </div>
     );
